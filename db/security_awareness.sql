ea0c69d7ade149e38381d14c8970f2e7815faaf2
Carlos Alberto <carlosdev1810@gmail.com>
2025-07-18 17:25:56 -0600

    feat(db): create security_awareness database and posts table

---
+++ db/security_awareness.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS  security_awareness;
+
+USE security_awareness;
+
+CREATE TABLE posts (
+  `id` INT AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `description` TEXT NOT NULL,
+  `filename` VARCHAR(255),
+  `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
