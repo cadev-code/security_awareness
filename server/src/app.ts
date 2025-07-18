d582b9b76e06e5e96113f7917d09609a1b6a000a
Carlos Alberto <carlosdev1810@gmail.com>
2025-07-18 17:37:29 -0600

    feat(server): install @types/cors package and configurate cors in app.ts file

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8089;
+
+app.use(cors());
 
 app.listen(PORT, () => {});
