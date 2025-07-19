80c1062394d895031453f04f00a7f64d95f179d6
Carlos Alberto <carlosdev1810@gmail.com>
2025-07-18 18:01:00 -0600

    feat(server): add endpoints to get posts and get audio

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,16 @@
-import dotenv from 'dotenv';
-import express from 'express';
 import cors from 'cors';
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { createPool } from 'mysql2/promise';
+import path from 'path';
+
+const pool = createPool({
+  host: 'db',
+  port: 3306,
+  user: 'root',
+  password: 'root',
+  database: 'security_awareness',
+});
 
 dotenv.config();
 
@@ -8,5 +18,33 @@ const app = express();
 const PORT = process.env.PORT || 8089;
 
 app.use(cors());
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/posts', async (req: Request, res: Response) => {
+  try {
+    const [result] = await pool.query(
+      'SELECT * FROM posts ORDER BY created_at DESC',
+    );
+    res.status(200).json({
+      error: null,
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      error,
+    });
+  }
+
+  res.status(200).json({
+    error: null,
+  });
+});
+
+app.use(
+  '/audio',
+  express.static(path.join(__dirname, '..', 'public', 'podcasts')),
+);
 
 app.listen(PORT, () => {});
