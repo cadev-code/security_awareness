5e2a2b77f5171b417cd3e2c65451602d634c3567
Carlos Escobedo <carlosdev1810@gmail.com>
2026-04-09 16:57:26 -0600

    feat(client): implement section fetching and display in Section component

--- client/src/pages/Section/Section.tsx
+++ client/src/pages/Section/Section.tsx
@@ -1,3 +1,108 @@
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router';
+
+import axios from 'axios';
+
+import type { Section as SectionType } from '@/types/section.types';
+
 export const Section = () => {
-  return <div>Section</div>;
+  const [section, setSection] = useState<SectionType | null>(null);
+
+  const [searchParams] = useSearchParams();
+  const sectionId = Number(searchParams.get('id'));
+
+  const getSection = async (sectionId: number) => {
+    try {
+      const url = `${import.meta.env.VITE_URL_API}/sections/${sectionId}`;
+      const response = await axios.get(url);
+      setSection(response.data);
+    } catch (error) {
+      setSection(null);
+      console.error('Error al obtener sección:', error);
+    }
+  };
+
+  useEffect(() => {
+    getSection(sectionId);
+  }, [sectionId]);
+
+  return (
+    <div className="w-full h-[calc(100vh)] flex items-center justify-center relative gap-12">
+      <img
+        className="absolute select-none z-0 w-full h-full left-0 top-0"
+        src={`${import.meta.env.VITE_URL_STATIC}/backgrounds/${section?.bg_url}`}
+        draggable={false}
+      />
+      {/* {page > 1 && (
+        <div
+          className="z-2 bg-[#012559] hover:text-cyan-200 rounded-lg cursor-pointer transition-all hover:scale-110"
+          onClick={() => {
+            if (page > 1) {
+              setSearchParams({ page: String(page - 1) });
+            }
+          }}
+        >
+          <SquareChevronLeft size={64} />
+        </div>
+      )} */}
+      {/* {videosData
+        .slice((page - 1) * itemsPerPage, page * itemsPerPage)
+        .map((data, i) => (
+          <div
+            key={i}
+            className="z-2"
+            onClick={() => {
+              const today = new Date();
+              const formattedAvailability = new Date(data.availability);
+              if (today >= formattedAvailability) {
+                if (
+                  data.title !== 'Exploradores de Riesgos (Bonus)' &&
+                  data.title !== 'Bonus 2: Newsletter'
+                ) {
+                  openVideoPlayer(data.filename, data.url_questions);
+                } else {
+                  openImageVisualizer(data);
+                }
+              }
+            }}
+          >
+            <VideoCard key={i} data={data} />
+          </div>
+        ))} */}
+      {/* {page < totalPages && (
+        <div
+          className="z-2 bg-[#002a58] hover:text-cyan-200 rounded-lg cursor-pointer transition-all hover:scale-110"
+          onClick={() => {
+            if (page < totalPages) {
+              setSearchParams({ page: String(page + 1) });
+            }
+          }}
+        >
+          <SquareChevronRight size={64} />
+        </div>
+      )} */}
+      {/* {videoToPlay.reproduce && (
+        <div className="fixed top-0 left-0 w-full h-[calc(100vh)] bg-black/60 flex justify-center items-center z-2">
+          <VideoPlayer
+            url_video={videoToPlay.url_video}
+            close={closeVideoPlayer}
+            url_questions={videoToPlay.url_questions}
+          />
+        </div>
+      )}
+      {imageToShow && (
+        <ImageVisualizer image={imageToShow} close={closeImageVisualizer} />
+      )} */}
+      <img
+        className="w-48 [@media(min-width:1400px)]:w-60 absolute right-10 bottom-6 z-1"
+        src="/images/logo.png"
+        draggable={false}
+      />
+      <img
+        className="w-86 [@media(min-width:1400px)]:w-120 absolute left-6 bottom-8 [@media(min-width:1400px)]:left-16 [@media(min-width:1400px)]:bottom-18 z-1"
+        src={`${import.meta.env.VITE_URL_STATIC}/subtitles/${section?.flag_url}`}
+        draggable={false}
+      />
+    </div>
+  );
 };
