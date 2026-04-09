260f29c4d3773f4f9dc7765f80d67046230f1efe
Carlos Escobedo <carlosdev1810@gmail.com>
2026-04-09 15:25:29 -0600

    feat(client): add Section component and integrate into Router

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -7,12 +7,14 @@ import { Videos_Psswrd } from '@/pages/Videos_Psswrd/Videos_Psswrd';
 import { Videos_Temporada3 } from '@/pages/Videos_Temporada3/Videos_Temporada3';
 import { Videos_Temporada4 } from '@/pages/Videos_Temporada4/Videos_Temporada4';
 import { Videos_Temporada5 } from '@/pages/Videos_Temporada5/Videos_Temporada5';
+import { Section } from '@/pages/Section/Section';
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="home" element={<Home />} />
       <Route path="information" element={<Information />} />
+      <Route path="section" element={<Section />} />
       <Route path="temporada-1" element={<Podcast />} />
       <Route path="temporada-2" element={<Videos />} />
       <Route path="oct-si" element={<NewsLetters />} />
