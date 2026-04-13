d36f882f6848c946e98229e71881368a30c806da
Carlos Escobedo <carlosdev1810@gmail.com>
2026-04-13 10:53:54 -0600

    feat(client): enhance Section page with pagination controls and chapter display

--- client/src/pages/Section/Section.tsx
+++ client/src/pages/Section/Section.tsx
@@ -6,12 +6,17 @@ import axios from 'axios';
 import type { Section as SectionType } from '@/types/section.types';
 import type { Chapter } from '@/types/chapter.types';
 import { ChapterCard } from './ChapterCard/ChapterCard';
+import { SquareChevronLeft, SquareChevronRight } from 'lucide-react';
 
 export const Section = () => {
   const [section, setSection] = useState<SectionType | null>(null);
   const [chapters, setChapters] = useState<Chapter[]>([]);
 
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = Number(searchParams.get('page') || 1);
+  const itemsPerPage = 4;
+  const totalPages = Math.ceil(chapters.length / itemsPerPage);
+
   const sectionId = Number(searchParams.get('id'));
 
   const getSection = async (sectionId: number) => {
@@ -55,52 +60,56 @@ export const Section = () => {
           draggable={false}
         />
       )}
-      {/* {page > 1 && (
+      {page > 1 && (
         <div
-          className="z-2 bg-[#012559] hover:text-cyan-200 rounded-lg cursor-pointer transition-all hover:scale-110"
+          className="z-2 text-gray-300 hover:text-white rounded-lg cursor-pointer transition-all hover:scale-110"
+          style={{ backgroundColor: section?.bg_color }}
           onClick={() => {
             if (page > 1) {
-              setSearchParams({ page: String(page - 1) });
+              setSearchParams(`?id=${sectionId}&page=${page - 1}`);
             }
           }}
         >
           <SquareChevronLeft size={64} />
         </div>
-      )} */}
-      {chapters.map((chapter) => (
+      )}
+      {chapters
+        .slice((page - 1) * itemsPerPage, page * itemsPerPage)
+        .map((chapter) => (
+          <div
+            key={chapter.id}
+            className="z-2"
+            onClick={() => {
+              const today = new Date();
+              const formattedAvailability = new Date(chapter.availability);
+              if (today >= formattedAvailability) {
+                // if (
+                //   data.title !== 'Exploradores de Riesgos (Bonus)' &&
+                //   data.title !== 'Bonus 2: Newsletter'
+                // ) {
+                //   openVideoPlayer(data.filename, data.url_questions);
+                // } else {
+                //   openImageVisualizer(data);
+                // }
+              }
+            }}
+          >
+            <ChapterCard chapter={chapter} />
+          </div>
+        ))}
+      {page < totalPages && (
         <div
-          key={chapter.id}
-          className="z-2"
-          onClick={() => {
-            const today = new Date();
-            const formattedAvailability = new Date(chapter.availability);
-            if (today >= formattedAvailability) {
-              // if (
-              //   data.title !== 'Exploradores de Riesgos (Bonus)' &&
-              //   data.title !== 'Bonus 2: Newsletter'
-              // ) {
-              //   openVideoPlayer(data.filename, data.url_questions);
-              // } else {
-              //   openImageVisualizer(data);
-              // }
-            }
-          }}
-        >
-          <ChapterCard chapter={chapter} />
-        </div>
-      ))}
-      {/* {page < totalPages && (
-        <div
-          className="z-2 bg-[#002a58] hover:text-cyan-200 rounded-lg cursor-pointer transition-all hover:scale-110"
+          className="z-2 text-gray-300 hover:text-white rounded-lg cursor-pointer transition-all hover:scale-110"
+          style={{ backgroundColor: section?.bg_color }}
           onClick={() => {
             if (page < totalPages) {
-              setSearchParams({ page: String(page + 1) });
+              setSearchParams(`?id=${sectionId}&page=${page + 1}`);
             }
           }}
         >
           <SquareChevronRight size={64} />
         </div>
-      )} */}
+      )}
       {/* {videoToPlay.reproduce && (
         <div className="fixed top-0 left-0 w-full h-[calc(100vh)] bg-black/60 flex justify-center items-center z-2">
           <VideoPlayer
