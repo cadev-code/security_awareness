0e6fa0909d461ec1cba72987fe073142dfa0fc1c
Carlos Escobedo <carlosdev1810@gmail.com>
2026-04-16 12:57:55 -0600

    feat(api): add ServeStaticModule for serving static files

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { PrismaModule } from './prisma/prisma.module';
 import { SectionsModule } from './sections/sections.module';
 import { ChaptersModule } from './chapters/chapters.module';
 
 @Module({
-  imports: [PrismaModule, SectionsModule, ChaptersModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'static'),
+      serveRoot: '/static',
+    }),
+    PrismaModule,
+    SectionsModule,
+    ChaptersModule,
+  ],
   controllers: [],
   providers: [],
 })
