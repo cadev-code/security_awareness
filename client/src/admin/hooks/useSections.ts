cbb25aad1cdc0196a5d68f30b2610989c413f1ab
Carlos Escobedo <carlosdev1810@gmail.com>
2026-04-20 10:03:09 -0600

    feat(client): add useSections hook for fetching section data

---
+++ client/src/admin/hooks/useSections.ts
@@ -0,0 +1,13 @@
+import { fetcher } from '@/api/queryHelpers';
+import { Section } from '@/types/section.types';
+import { useQuery } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+
+export const useSections = () => {
+  return useQuery<Section, AxiosError>({
+    queryKey: ['sections'],
+    queryFn: () => fetcher<Section>('/sections'),
+    retry: false,
+    staleTime: 1000 * 30, // 5 minutes
+  });
+};
